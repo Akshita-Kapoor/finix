daf3dd62ed65f5972e0da3d4791666b79c0d2ec5
Akshita <ak.akshita21@gmail.com>
2026-01-06 15:22:56 -0700

    package files

--- tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./index.html', './src/**/*.{ts,tsx}'],
-    theme: { extend: {} },
-    plugins: [require('@tailwindcss/forms')],
-  };
-  
